d6dea66b9993f729b0d4531a74fe5ab26c4f4f9f
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-25 16:39:16 -0700

    fixed an error with AboutMe

--- cslockwood.portfolio/src/AboutMe.js
+++ cslockwood.portfolio/src/AboutMe.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function AboutMe() {
     return (
+      <div>
       <h2>About Me</h2>
       {/* Picture goes here */}
       <p>ipsa lorem ect ect</p>
+      </div>
     );
 }
   
