6d9f8ea688e2aa3f9bceeae1375ef0120464fd28
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-25 16:10:40 -0700

    tested header --working

--- cslockwood.portfolio/src/App.js
+++ cslockwood.portfolio/src/App.js
@@ -1,2 +1,14 @@
 import React from 'react';
-import Header from './Header';
+import Header from './header.js';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* Add other components for the About Me, Portfolio, Contact, and Resume sections here */}
+    </div>
+  );
+}
+
+export default App;
