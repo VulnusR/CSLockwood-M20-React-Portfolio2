074df3e3126337ae0c0e469b39b4ae986d9a44a3
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-03-26 16:22:38 -0700

    resume updated

--- cslockwood.portfolio/src/Resume.js
+++ cslockwood.portfolio/src/Resume.js
@@ -2,11 +2,26 @@ import React from 'react';
 
 function Resume() {
     return (
-      <div className='main-article'>
+    <div className='main-article'>
       <h2 className='page-title'>Resume</h2>
-      {/* resume goes here */}
+      <h3  className="contact-h3">Front End Proficiencies</h3>
+      <p>HTML</p>
+      <p>CSS</p>
+      <p>JavaScript</p>       
+      <p>JQuery</p>
+      <p>Responsive Design</p>
+      <p>Bootstrap</p>
+      <br></br>        
+      <h3 className="contact-h3">Back End Proficiencies</h3>
+      <p>APIs</p>
+      <p>Node</p>
+      <p>Express</p>        
+      <p>MySql, Sequelize</p>
+      <p>MongoDB, Mongoose</p>
+      <p>REST</p>
+      <p>GraphicQL</p>
       </div>
-    );
+  );
 }
   
 export default Resume;
