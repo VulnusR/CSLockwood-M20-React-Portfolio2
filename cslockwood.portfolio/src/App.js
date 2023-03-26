1bfb517f3ffe8a104014d9a7db22db6e55aa9d07
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-25 20:50:47 -0700

    using react router and updated app.js to accomodate

--- cslockwood.portfolio/src/App.js
+++ cslockwood.portfolio/src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './header.js';
 import AboutMe from './AboutMe.js';
 import Portfolio from './Portfolio.js';
 import Contact from './Contact.js';
 import Resume from './Resume.js';
 import Footer from './Footer.js';
-
 import './Styles/styles.css';
-
 
 function App() {
   return (
-    <div className = "parentdiv">
-      <Header />
-      <AboutMe />
-      <Portfolio />
-      <Contact />
-      <Resume /> 
-      <Footer />
-    </div>
+    <Router>
+      <div className="parentdiv">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={AboutMe} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
