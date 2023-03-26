9e8aacdf2cf167d1e94a0c2b609b03c1573edb2f
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-03-26 16:43:13 -0700

    resume style update

--- cslockwood.portfolio/src/Resume.js
+++ cslockwood.portfolio/src/Resume.js
@@ -4,23 +4,34 @@ function Resume() {
     return (
     <div className='main-article'>
       <h2 className='page-title'>Resume</h2>
-      <h3  className="contact-h3">Front End Proficiencies</h3>
-      <p>HTML</p>
-      <p>CSS</p>
-      <p>JavaScript</p>       
-      <p>JQuery</p>
-      <p>Responsive Design</p>
-      <p>Bootstrap</p>
-      <br></br>        
-      <h3 className="contact-h3">Back End Proficiencies</h3>
-      <p>APIs</p>
-      <p>Node</p>
-      <p>Express</p>        
-      <p>MySql, Sequelize</p>
-      <p>MongoDB, Mongoose</p>
-      <p>REST</p>
-      <p>GraphicQL</p>
+      <h4 className='download-txt'>Download my resume<span className='download-link'> here!</span></h4>
+      <div className='resume-parent'>
+        <div className='frontend-parent'>
+          <h3  className="contact-h3">Front End Proficiencies</h3>
+          <div className='resume-p'>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>JavaScript</p>       
+            <p>JQuery</p>
+            <p>Responsive Design</p>
+            <p>Bootstrap</p>
+          </div>
+        </div>  
+           
+        <div className='backend-parent'>
+          <h3 className="contact-h3">Back End Proficiencies</h3>
+          <div className='resume-p'>
+            <p>APIs</p>
+            <p>Node</p>
+            <p>Express</p>        
+            <p>MySql, Sequelize</p>
+            <p>MongoDB, Mongoose</p>
+            <p>REST</p>
+            <p>GraphicQL</p>
+          </div>
+        </div> 
       </div>
+    </div>
   );
 }
   
