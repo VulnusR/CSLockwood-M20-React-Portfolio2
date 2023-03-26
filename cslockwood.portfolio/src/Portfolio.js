c405df3198cc1f27e93041b3cf7b93ae98811163
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-03-26 16:58:40 -0700

    basic project/portfolio structure

--- cslockwood.portfolio/src/Portfolio.js
+++ cslockwood.portfolio/src/Portfolio.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Project from './Project.js';
 
 function Portfolio() {
     return (
       <div className='main-article'>
       <h2 className='page-title'>Portfolio</h2>
-      {/* Projects goes here */}
+      <div className='portfolio-parent'>
+        <div className='porfolio-col-1'>
+          <Project></Project>
+          <Project></Project>
+          <Project></Project>
+
+        </div>
+        <div className='porfolio-col-2'>
+        <Project></Project>
+        <Project></Project>
+        <Project></Project>
+        
+          
+        </div>
+      </div>
       </div>
     );
 }
---
+++ cslockwood.portfolio/src/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Project() {
+    return (
+      <div className='project-parent'>
+        <p>sdfgsfdgsfdg</p>
+
+      </div>
+    );
+}
+
+export default Project;
