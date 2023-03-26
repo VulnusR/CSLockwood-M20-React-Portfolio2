40a9b1e9f88d301a9622458c1150d80b965a6ae8
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-03-26 13:37:34 -0700

    various mainly footer/icon changes, about me page is p much done only problem is scroll behavior cuts off the top of text in certain viewports

--- cslockwood.portfolio/src/Footer.js
+++ cslockwood.portfolio/src/Footer.js
@@ -6,19 +6,19 @@ function Footer() {
     return (
       <footer className = "Footer-Box">
         <ul className = "Footer-content-Parent">
-          <li>
+          <li className='icon'>
             <a href="https://github.com/VulnusR">
-              <FaGithub />
+              <FaGithub className='iconcolor' />
             </a>
           </li>
-          <li>
-            <a href="https://www.linkedin.com/in/caleb-lockwood-3106a0236/">
-              <FaLinkedin />
+          <li className='icon'>
+            <a href="https://www.linkedin.com/in/caleb-lockwood-3106a0236/" >
+              <FaLinkedin className='iconcolor' />
             </a>
           </li>
-          <li>
-            <a href="https://twitter.com/VulnusR">
-              <FaTwitter />
+          <li className='icon'>
+            <a href="https://twitter.com/VulnusR" >
+              <FaTwitter className='iconcolor' />
             </a>
           </li>
         </ul>
