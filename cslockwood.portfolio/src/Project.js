25c57e6ae5225e207fc98be2f52b05129f78599c
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-03-27 17:07:02 -0700

    css styling for portfolio

--- cslockwood.portfolio/src/Project.js
+++ cslockwood.portfolio/src/Project.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Project(props) {
   return (
     <div className='project-parent'>
-      <img src={props.picture} alt='project' />
+      <img src={props.picture} alt='project' className='project-img' />
       <p>{props.description}</p>
       <a href={props.link}>View project</a>
     </div>
